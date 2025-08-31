223013f485939c8792c2e06c311449e3200cd0bd
Harry-Daniel <hayiborkomla@gmail.com>
2025-08-31 21:15:31 +0000

    Made some changes to order processing page

--- app/orderPreparing/index.jsx
+++ app/orderPreparing/index.jsx
@@ -1,25 +1,26 @@
 import { View, Text, Dimensions } from "react-native";
-import bikeguy from "../../assets/images/bikeguy.gif";
+import bikeguy from "../../assets/images/bikeguy2.gif";
 import { Image } from "expo-image";
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
 
 const { height } = Dimensions.get("screen");
 const OrderPreparingScreen = () => {
-    const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    setTimeout(()=>{
-router.push({pathname:"/delivery"})
-    },5000)
-  }, [])
-  
+    setTimeout(() => {
+      router.push({ pathname: "/delivery" });
+    }, 5000);
+  }, []);
 
   return (
     <View
-      className=" bg-white justify-center items-center"
+      className=" bg-white justify-center items-center gap-10"
       style={{ height: height }}
     >
       <Image source={bikeguy} style={{ width: 200, height: 200 }} />
+
+      <Text className="font-bold text-lg text-gray-300">Preparing Order . . .</Text>
     </View>
   );
 };
